24e6b466af8207daa1b2b6243541091bff304687
sirBruh <finsta32@kingshigh.school.nz>
2024-06-21 13:12:00 +1200

    Player Mechanics

---
+++ Scenes/Player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+@export var speed = 30
+@export var max_speed = 300
+@export var friction = 10
+@export var jump_velocity = -400
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * max_speed, speed)
+	else:
+		velocity.x = move_toward(velocity.x, 0,friction)
+	if velocity.x > 0:
+		$AnimatedSprite2D.play("run")
+	elif velocity.x == 0:
+		$AnimatedSprite2D.play("idle")
+	#changing direction
+	if direction > 0:
+		$AnimatedSprite2D.flip_h = true
+	elif direction < 0:
+		$AnimatedSprite2D.flip_h = false
+	move_and_slide()
