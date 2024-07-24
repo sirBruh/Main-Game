162dc53367a5fd94be59652e7513dc5c0e6db9bc
sirBruh <finsta32@kingshigh.school.nz>
2024-07-24 13:12:59 +1200

    Menu & Bullet work-ons

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -1,5 +1,7 @@
 extends Control
-
+@onready var sprite1 = $Play/AnimatedSprite2D_Play
+@onready var sprite2 = $Play/AnimatedSprite2D_How_to_Play
+@onready var sprite3 = $Play/AnimatedSprite2D_Quit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,12 +14,14 @@ func _process(delta):
 
 
 func _on_play_pressed():
+	sprite1.play("Pressed")
 	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 
 
 func _on_exit_pressed():
+	sprite3.play("Pressed")
 	get_tree().quit()
 
 
 func _on_how_to_play_pressed():
-	pass # Replace with function body.
+	sprite2.play("Pressed")
