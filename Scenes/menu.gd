2196b1a53dbfa867b722e4f0d19d8e4bed4cca41
sirBruh <finsta32@kingshigh.school.nz>
2024-07-24 13:30:20 +1200

    More menu shenanigans

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -1,7 +1,5 @@
 extends Control
-@onready var sprite1 = $Play/AnimatedSprite2D_Play
-@onready var sprite2 = $Play/AnimatedSprite2D_How_to_Play
-@onready var sprite3 = $Play/AnimatedSprite2D_Quit
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,14 +12,14 @@ func _process(delta):
 
 
 func _on_play_pressed():
-	sprite1.play("Pressed")
+	await get_tree().create_timer(.5).timeout
 	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 
 
 func _on_exit_pressed():
-	sprite3.play("Pressed")
+	await get_tree().create_timer(.5).timeout
 	get_tree().quit()
 
 
 func _on_how_to_play_pressed():
-	sprite2.play("Pressed")
+	await get_tree().create_timer(.5).timeout
