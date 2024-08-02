6696a35c68a2b4323fcb6b9ba860932faa35b24e
sirBruh <finsta32@kingshigh.school.nz>
2024-08-02 14:35:53 +1200

    Dying enemy

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -3,6 +3,7 @@ extends Area2D
 @export var right_limit = 0
 var direction = 1
 @onready var sprite = $AnimatedSprite2D
+var dead = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,6 +11,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if dead:
+		return
 	translate(Vector2.RIGHT * direction)
 	if direction < 0:
 		sprite.flip_h = true
@@ -20,5 +23,7 @@ func _process(delta):
 	elif position.x < left_limit:
 		direction = 1
 
-
-
+func _on_hitbox_area_entered(area):
+	if area.is_in_group("Enemy_Death"):
+		dead = true
+		sprite.play("die")
